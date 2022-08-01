96e9c4b888218b1663740425525a701348370731
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2022-08-01 23:24:45 +0530

    Added js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hello");
+var btnTranslate = document.querySelector("#btn-translate");
+
+var txtInput = document.querySelector("#txt-input");
+
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURl(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function handleClick() {
+    // console.log(txtInput.value)
+    fetch(getTranslationURl(txtInput.value))
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            // var translatedText = json.contents.translated;
+            // outputDiv.innerText = translatedText;
+        })
+        .catch(err => console.log(err))
+
+    // console.log("Clicked");
+}
+
+btnTranslate.addEventListener("click", handleClick)
