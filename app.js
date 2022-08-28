df9f0a099dc697818a86987dc37ada71757e6b3a
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2022-08-28 18:27:38 +0530

    Update app.js
    added error handling code

--- app.js
+++ app.js
@@ -4,24 +4,32 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+var serverURL = "https://api.funtranslations.com/translate/pirate.json";
 
 function getTranslationURl(text) {
     return serverURL + "?" + "text=" + text;
 }
 
-function handleClick() {
+async function handleClick() {
     // console.log(txtInput.value)
-    fetch(getTranslationURl(txtInput.value))
-        .then(response => response.json())
-        .then(json => {
-            // console.log(json)
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText;
-        })
-        .catch(err => alert(err.message))
 
-    // console.log("Clicked");
+    if (txtInput.value == "") {
+        alert(
+            "Please enter the text/sentence that you want to translate to banana language"
+        );
+    } else {
+        try {
+            const res = await fetch(getTranslationURl(txtInput.value));
+            const data = await res.json();
+            if (res.status === 429) {
+                throw new Error(data.error.message);
+            }
+            const translatedText = data.contents.translated;
+            outputText.textContent = translatedText;
+        } catch (err) {
+            alert(err);
+        }
+    }
 }
 
-btnTranslate.addEventListener("click", handleClick)
+btnTranslate.addEventListener("click", handleClick);
